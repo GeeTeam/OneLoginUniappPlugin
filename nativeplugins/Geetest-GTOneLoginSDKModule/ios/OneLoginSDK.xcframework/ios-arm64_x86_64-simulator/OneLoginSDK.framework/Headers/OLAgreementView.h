e0f8232455e6d8577e090bafc0423e897e73d849
guoqing <guoqing@geetest.com>
2025-04-11 14:15:47 +0800

    更新 iOS 原生SDK 为 2.9.7

---
+++ nativeplugins/Geetest-GTOneLoginSDKModule/ios/OneLoginSDK.xcframework/ios-arm64_x86_64-simulator/OneLoginSDK.framework/Headers/OLAgreementView.h
@@ -0,0 +1,41 @@
+//
+//  OLAgreementView.h
+//  OneLoginSDK
+//
+//  Created by NikoXu on 2019/6/11.
+//  Copyright © 2019 geetest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OLPrivacyTermItem.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol OLAgreementViewDelegate;
+
+@interface OLAgreementView : UITextView
+
+@property (nonatomic, weak) id<OLAgreementViewDelegate> agreementDelegate;
+
+- (void)updateTermItems:(NSArray<OLPrivacyTermItem *> *)termItems
+          withTextColor:(UIColor *)textColor
+              termsAttr:(NSDictionary<NSAttributedStringKey, id> *)termsAttr
+  auxiliaryPrivacyWords:(NSArray<NSString *> *)auxiliaryPrivacyWords
+          textAlignment:(NSTextAlignment)textAlignment;
+
+- (void)updateTermItems:(NSArray<OLPrivacyTermItem *> *)termItems
+          withTextColor:(UIColor *)textColor
+              termsAttr:(NSDictionary<NSAttributedStringKey, id> *)termsAttr
+  auxiliaryPrivacyWords:(NSArray<NSString *> *)auxiliaryPrivacyWords
+          textAlignment:(NSTextAlignment)textAlignment
+                   font:(UIFont *)font;
+
+@end
+
+@protocol OLAgreementViewDelegate <NSObject>
+
+- (BOOL)agreementView:(OLAgreementView *)agreementView shouldInteractWithURL:(NSURL *)URL;
+
+@end
+
+NS_ASSUME_NONNULL_END
